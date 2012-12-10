d73d35a307a756b7e2bfd21179205016cac6f27c
Lucas Bickel <hairmare@purplehaze.ch>
2012-12-10 23:00:15 +0100

    need rspec-hiera-puppet to get through this first set of testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,21 @@
 require 'rubygems'
+require 'rspec-hiera-puppet'
 require 'puppetlabs_spec_helper/module_spec_helper'
 
 RSpec.configure do |c|
   c.filter_run_excluding :broken => true
 end
+
+fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
+
+shared_context "hieradata" do
+  let(:hiera_config) do
+    { :backends => ['rspec', 'yaml'],
+      :hierarchy => [
+        '%{calling_module}'],
+      :yaml => {
+        :datadir => File.join(fixture_path, 'hieradata') },
+      :rspec => respond_to?(:hiera_data) ? hiera_data : {} }
+  end
+end
+
